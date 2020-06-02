0a62753c95bc9a921b97c0c0791e3e284fdb042e
Pablo Cirant <cirant3002@gmail.com>
2020-06-02 13:53:32 -0400

    icons test

--- src/buttons/index.js
+++ src/buttons/index.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import styles from '../scss/_buttons.scss';
+// import '../scss/fonts/'
 
 const Button = ({ children, size, color, variant, disabled }) => {
   const btnSize = size === 'large' ? 'large' : 'regular';
--- src/icons/index.js
+++ src/icons/index.js
@@ -2,18 +2,17 @@ import React, { Fragment } from 'react';
 import path from 'path';
 import iconss from '../out/css/sprite.css'
 
-// import * as ico from './';
 
 const Icon = ({ name = 'alarm' }) => {
-  // const file = path.resolve(`./${name}.svg`);
-  // console.log(ico);
-  console.log(iconss);
+  const file = path.resolve(`./${name}.svg`);
+  console.log(file);
+  // console.log(iconss);
 
 
   // const imagex = require(`./${name}.svg`);
   return <>
     <div style={{ height: '32px', width: '32px' }} width="25" className={iconss[`svg-${name}`]} />
-    {/* <img width="25" className={iconss[`svg-${name}`]} /> */}
+    <img width="25" src={file} />
   </>
 }
 
