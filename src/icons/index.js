111befe255c672f73713f9b074d46903a1ffa28e
Pablo Cirant <cirant3002@gmail.com>
2020-06-02 16:41:14 -0400

    test

--- src/icons/index.js
+++ src/icons/index.js
@@ -1,8 +1,9 @@
-import React, { Fragment } from 'react';
+import React from 'react';
 import icons from '../scss/_coopeuch-icon.scss';
 
 const Icon = ({ name = 'alarm' }) => {
-  return <div width="25" className={icons[`icon-${name}`]} />
+  const icon = icons[`icon-${name}`];
+  return <div width="25" className={`coopeuch-icon ${icon}`} />
 }
 
 export default Icon;
